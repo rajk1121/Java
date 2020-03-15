ba499573eb2e130e501adc106f2cceaf75db3cfa
rajk1121intellify <rajat.mishra1121@gmail.com>
2020-03-15 12:04:52 +0530

    Combination_Sum

--- Combination_Sum.java
+++ Combination_Sum.java
@@ -1,17 +1,14 @@
 import java.util.*;
+//Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
 public class Combination_Sum {
-//	static List<List<Integer>> solution = new ArrayList();
 	 public static List<List<Integer>>  combinationSum(int[] candidates, int target,List<Integer> isol, int sum, List<List<Integer>> solution, int index) {
 		 if(index>candidates.length || sum>target) {
 			 return null;
 		 }
 		 if(sum==target) {
 			 List<Integer> isoo = new ArrayList<>();
-//			 isoo = isol;
 			 isoo.addAll(isol);
 			 solution.add(isoo);
-//			 System.out.println(solution);
-//			 System.out.println(isol);
 			 return solution;
 		 }
 		 while(index<candidates.length) {
@@ -22,8 +19,6 @@ public class Combination_Sum {
 			 }else {
 				 index++;
 			 }
-//			 System.out.println(solution);
-//			 System.out.println(isol.get(isol.size()-1));
 			 isol.remove(isol.size()-1);
 		 }
 		 return solution ;
