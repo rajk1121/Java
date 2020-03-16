852118ddf23b908b815f5fb56f29f91e619a0226
rajk1121intellify <rajat.mishra1121@gmail.com>
2020-03-16 17:37:50 +0530

    permutations

---
+++ permutation.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class permutation {
+	 public static List<List<Integer>> per(int[] nums, int si, int mi, int ei,List<Integer> isol,int number,HashMap<Integer,Integer> set){
+	 
+		 List<List<Integer>> solution = new ArrayList<>();
+		 if(si>ei || number>nums.length-1) {
+			 List<Integer> isolu = new ArrayList<>();
+			 isolu.addAll(isol);
+//			 System.out.println(isolu);
+			 solution.add(isolu);
+			 return solution;
+		 }
+		 for(int i=si ; i<=ei ; i++) {
+			 if(i==mi && i!=si || set.containsKey(nums[i])) {
+				 continue;
+			 }
+//			 List<Integer> answer = new ArrayList();
+			 List<List<Integer>> isolution = new ArrayList<>();
+			 isol.add(nums[i]);
+			 set.put(nums[i], 1);
+			 if(i==si) {
+				 if(mi==si) {
+					 isolution = per(nums, si+1, mi+1, ei, isol, number+1,set);
+				 }else {
+					 isolution = per(nums, si+1, mi, ei, isol, number+1, set);
+				 }
+			 }else {
+				  isolution = per(nums, si, i, ei, isol, number+1,set);
+				 
+			 }
+			 for(int j=0 ; j<isolution.size();j++) {
+				 solution.add(isolution.get(j));
+			 }
+			 isol.remove(isol.size()-1);
+			 set.remove(nums[i]);
+		 }
+		 return solution;   
+	 }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] nums = {1,2,3,4,5};
+		HashMap<Integer,Integer> set = new HashMap<>();
+		System.out.println(per(nums,0,0,nums.length-1,new ArrayList<>(),0,set));
+
+	}
+
+}
