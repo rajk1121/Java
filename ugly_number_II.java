f7b0a8fbf6305bb36b5500718a45bc261d3d222f
rajk1121 <rajat.mishra1121@gmail.com>
2020-04-04 17:40:51 +0530

    DPnthUglyNumber

--- ugly_number_II.java
+++ ugly_number_II.java
@@ -45,7 +45,35 @@ public class ugly_number_II {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(nthUglyNumber(100));
+		System.out.println(nthUglyNumber(200));
+		System.out.println(DPnthUglyNumber(200));
 	}
+	 public static long DPnthUglyNumber(int n) {
+		 if(n==1) return 1;
+		int[] uglyNo = new int[n];
+		int x =0;
+		uglyNo[0]= 1;
+		int i2=0,i3=0,i5=0;
+		int m2 = uglyNo[i2]*2,m3=uglyNo[i3]*3,m5=uglyNo[i5]*5;
+		for(int i=1 ; i<n ;i++) {
+			int min = Math.min(m2, Math.min(m3, m5));
+			uglyNo[i] = min;
+			System.out.println(min);
+			if(min==m2) {
+				i2++;
+				m2 = uglyNo[i2]*2;
+				
+			} if(min==m3) {
+				i3++;
+				m3 = uglyNo[i3]*3;
+				
+			} if(min==m5) {
+				i5++;
+				m5 = uglyNo[i5]*5;
+				
+			}
+		}
+		 return uglyNo[n-1];
+	 }
 
 }
