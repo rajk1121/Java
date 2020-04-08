e49c6cb02cb31846a90234771edb9b0c24cf26a5
rajk1121 <rajat.mishra1121@gmail.com>
2020-04-08 11:00:03 +0530

    DP_coin_change

--- coin_change.java
+++ coin_change.java
@@ -1,15 +1,12 @@
 import java.util.*;
 public class coin_change {
 	 public static int coinChange(int[] coins, int amount) {
-	     Arrays.sort(coins);
+//	     Arrays.sort(coins);
 		 if(coins.length==0)
 	    	 return 0;
 	     int[] dp = new int[amount+1];
 	     Arrays.fill(dp, -1);
 	    int ans = find(coins, amount,0,0,0,dp);
-//	    for(int i=0 ; i<dp.length ; i++)
-//	    	System.out.print(dp[i]+" ");
-//	    System.out.println();
 	    return ans;
 	    }
 	private static int find(int[] coins, int amount, int i, int sum, int count, int[] dp) {
@@ -48,17 +45,41 @@ public class coin_change {
 			return -1;
 		}else {
 
-			if(dp[sum]!=-1 && dp[sum]!=min-count)
-				System.out.println(dp[sum]+" "+(min-count)+" "+sum);
+//			if(dp[sum]!=-1 && dp[sum]!=min-count)
+//				System.out.println(dp[sum]+" "+(min-count)+" "+sum+" "+dp[amount-sum]);
 			dp[sum] = min-count;
 			return min;
 		}
 	}
+public static int  DPcoinChange(int[] coins, int amount){
+		if(coins.length==0 || amount==0)
+			return 0;
+		int[]dp = new int[amount+1];
+		Arrays.fill(dp, amount+1);
+		dp[0]=0;
+		for(int i=1 ; i<=amount; i++) {
+			
+			for(int j=0 ; j<coins.length ; j++) {
+				if(coins[j]<=i) {
+//					System.out.println(coins[j]);
+					dp[i]= Math.min(dp[i], 1+dp[i-coins[j]]);
+//					System.out.println(dp[i]+" "+coins[j]+" "+i);
+				}
+			}
+//			System.out.print(dp[i]+" ");
+		}
+
+//	    for(int i=0 ; i<=amount ;i++)
+//	        System.out.println(dp[i]);
+        return dp[amount]>amount?-1:dp[amount];
+        
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] coins = {58,92,387,421,194,208,231};
-		Arrays.sort(coins);
-		System.out.println(coinChange(coins,7798));
+		int[] coins = {186,419,83,408};
+//		int[] coins = {1,2,5};
+//		Arrays.sort(coins);
+		System.out.println(DPcoinChange(coins,6249));
 	}
 
 }
