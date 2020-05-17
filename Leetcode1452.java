b56fad35c19608a4cedf7ad07e958074b671328f
rajk1121 <rajat.mishra1121@gmail.com>
2020-05-17 20:13:55 +0530

    rearrange_words_in_array

--- Leetcode1452.java
+++ Leetcode1452.java
@@ -1,3 +1,20 @@
+/*
+ * Given the array favoriteCompanies where favoriteCompanies[i] is the list of favorites companies for the ith person (indexed from 0).
+
+Return the indices of people whose list of favorite companies is not a subset of any other list of favorites companies. You must return the indices in increasing order.
+
+ 
+
+Example 1:
+
+Input: favoriteCompanies = [["leetcode","google","facebook"],["google","microsoft"],["google","facebook"],["google"],["amazon"]]
+Output: [0,1,4] 
+Explanation: 
+Person with index=2 has favoriteCompanies[2]=["google","facebook"] which is a subset of favoriteCompanies[0]=["leetcode","google","facebook"] corresponding to the person with index 0. 
+Person with index=3 has favoriteCompanies[3]=["google"] which is a subset of favoriteCompanies[0]=["leetcode","google","facebook"] and favoriteCompanies[1]=["google","microsoft"]. 
+Other lists of favorite companies are not a subset of another list, therefore, the answer is [0,1,4].
+
+ */
 import java.util.*;
 class listComps implements Comparator<List<String>>{
     public int compare(List<String> a, List<String> b){
