15fcfd49e95feb1587b38f116394ec2aa5eec373
rajk1121 <rajat.mishra1121@gmail.com>
2020-05-23 11:24:29 +0530

    foobbar3_a

--- foobar3_a.java
+++ foobar3_a.java
@@ -6,32 +6,35 @@ public class foobar3_a {
         BigInteger ans = new BigInteger("0");
 //        int z = X.compareTo(Y);
         BigInteger one = new BigInteger("1");
+//        System.out.println(X.compareTo(Y));
         while(X.compareTo(one)==1 && Y.compareTo(one)==1 ){
         	
-        
-            if(X.compareTo(Y)<0){
-                Y = Y.subtract(X.multiply(Y.divide(X)));
-            }else if(Y.compareTo(X)<0){
+        	
+            if(X.compareTo(Y)==-1){
+            	ans = ans.add(Y.divide(X));
+            	Y = Y.subtract(X.multiply(Y.divide(X)));
+            }else if(Y.compareTo(X)==-1){
+            	 ans = ans.add(X.divide(Y));
                 X = X.subtract(Y.multiply(X.divide(Y)));
             }else{
                 break;
             }
-            ans = ans.add(one);
 //            System.out.println(X+" "+Y);
         }
+        System.out.println("ddeded");
         if(X.compareTo(one)==0)
         	ans = ans.add(Y.subtract(X));
         if(Y.compareTo(one)==0)
         	ans = ans.add(X.subtract(Y));
         if(X.compareTo(one) == 0 || Y.compareTo(one)==0)
-           return ans.toString();
+           return String.valueOf(ans);
        
        return "Impossible";
         
    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(solution("1", "2"));
+		System.out.println(solution("111", "2"));
 	}
 
 }
--- sort_frequency.java
+++ sort_frequency.java
@@ -1,31 +1,31 @@
 import java.util.*;
-class Node{
+class SortNode{
     char x;
     int size;
-    public Node(char a){
+    public SortNode(char a){
         x = a;
         size = 1;
     }
 }
-class ListComp implements Comparator<Node>{
-    public int compare(Node a, Node b){
+class ListComp implements Comparator<SortNode>{
+    public int compare(SortNode a, SortNode b){
         return a.size - b.size ;
     }
 }
 
 class sort_frequency {
     public String frequencySort(String s) {
-        List<Node> list = new ArrayList<>();
-        HashMap<Character, Node> set = new HashMap<>();
+        List<SortNode> list = new ArrayList<>();
+        HashMap<Character, SortNode> set = new HashMap<>();
         for(int i=0 ; i<s.length() ; i++){
             if(set.containsKey(s.charAt(i))){
                 set.get(s.charAt(i)).size = set.get(s.charAt(i)).size + 1; 
             }else{
-                Node n = new Node(s.charAt(i));
+                SortNode n = new SortNode(s.charAt(i));
                 set.put(s.charAt(i), n);
             }
         }
-        for(Map.Entry<Character, Node> l:set.entrySet()){
+        for(Map.Entry<Character, SortNode> l:set.entrySet()){
             list.add(l.getValue());
         }
         Collections.sort(list, new ListComp());
